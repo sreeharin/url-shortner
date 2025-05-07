6ee659aa7abc0c6ec8899b382df7df0e98fa0b32
shn <sreeharinagesh@gmail.com>
2025-05-07 05:35:00 +0000

    updated auth middleware

--- internal/middleware/auth.go
+++ internal/middleware/auth.go
@@ -2,15 +2,26 @@ package middleware
 
 import (
 	"net/http"
+	"strings"
 
 	"github.com/gin-gonic/gin"
 	"github.com/golang-jwt/jwt/v5"
+
+	"github.com/sreeharin/url-shortner/internal/models"
 )
 
 func Auth() gin.HandlerFunc {
 	return func(c *gin.Context) {
-		tokenString := "dummy"
-		token, err := jwt.Parse(tokenString, func(t *jwt.Token) (any, error) {
+		authHeader := c.GetHeader("Authorization")
+
+		if !strings.HasPrefix(authHeader, "Bearer") {
+			c.AbortWithStatusJSON(http.StatusUnauthorized, gin.H{"error": "Authorization header missing or malformed"})
+			return
+		}
+
+		tokenString := strings.TrimPrefix(authHeader, "Bearer ")
+
+		token, err := jwt.ParseWithClaims(tokenString, &models.CustomClaim{}, func(t *jwt.Token) (any, error) {
 			return []byte(`secret`), nil
 		})
 
--- internal/middleware/auth_test.go
+++ internal/middleware/auth_test.go
@@ -1,19 +1,21 @@
 package middleware
 
 import (
-	"log"
+	"fmt"
 	"net/http"
 	"net/http/httptest"
 	"testing"
+	"time"
 
 	"github.com/gin-gonic/gin"
+	"github.com/golang-jwt/jwt/v5"
+	"github.com/sreeharin/url-shortner/internal/models"
 )
 
 func TestAuth(t *testing.T) {
 	router := gin.New()
-	router.Use(Auth())
 
-	router.GET("/", func(c *gin.Context) {
+	router.GET("/", Auth(), func(c *gin.Context) {
 		c.JSON(200, gin.H{"message": "Hello, World!"})
 	})
 
@@ -23,10 +25,30 @@ func TestAuth(t *testing.T) {
 
 		router.ServeHTTP(w, req)
 
-		log.Println(w.Body.String())
-
 		if w.Code != http.StatusUnauthorized {
 			t.Errorf("Expected status code: %d got: %d", http.StatusUnauthorized, w.Code)
+		}
+	})
+
+	t.Run("Authorized", func(t *testing.T) {
+		claims := models.CustomClaim{
+			User: 1,
+			RegisteredClaims: jwt.RegisteredClaims{
+				ExpiresAt: jwt.NewNumericDate(time.Now().Add(5 * time.Minute)),
+			},
+		}
+
+		token := jwt.NewWithClaims(jwt.SigningMethodHS256, claims)
+		tokenString, _ := token.SignedString([]byte("secret"))
+
+		w := httptest.NewRecorder()
+		req, _ := http.NewRequest("GET", "/", nil)
+		req.Header.Add("Authorization", fmt.Sprintf("Bearer %s", tokenString))
+
+		router.ServeHTTP(w, req)
+
+		if w.Code != http.StatusOK {
+			t.Errorf("Expected status code: %d got: %d", http.StatusOK, w.Code)
 		}
 	})
 
